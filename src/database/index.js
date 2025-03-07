3cd2e34c09de51bf126b7f07b3eb4b93c9d1b53a
sagorsohel <sohel19sagor@gmail.com>
2025-03-08 04:17:03 +0600

    feat: implement Express app and enhance MongoDB connection handling

--- src/database/index.js
+++ src/database/index.js
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 const connectMongoBD = async () => {
   try {
     const mongoUri = process.env.MONGODB_URI;
-    // if (!mongoUri.startsWith("mongodb://") && !mongoUri.startsWith("mongodb+srv://")) {
-    //   throw new Error("Invalid MongoDB URI scheme");
-    // }
+    
     const connectionInstance = await mongoose.connect(
       `${mongoUri}/${process.env.MONGO_DB_NAME}`
     );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import dotenv from "dotenv";
 import connectMongoBD from "./database/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "/.env",
 });
 
-
-
-connectMongoBD();
+connectMongoBD()
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 5000}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
