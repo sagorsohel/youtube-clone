1996131e87fc176460f898192358210ab65ec900
sagorsohel <sohel19sagor@gmail.com>
2025-03-09 04:05:33 +0600

    feat: enhance user registration with validation and file upload support

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,24 @@
+import User from "../models/user.model.js";
+import apiErrors from "../utils/apiErrors.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
-export const registerUser = asyncHandler(async (resizeBy, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Register User",
-  });
-});
+export const registerUser = asyncHandler(async (req, res, next) => {
+  const { name, email, password } = req.body;
 
+  //   check required fields
+  const missingFields = ["name", "email", "password"].filter(
+    (field) => !req.body[field]
+  );
+
+  if (missingFields.length) {
+    throw new apiErrors(400, `Missing fields: ${missingFields.join(", ")}`);
+  }
+
+  //   check if user already exists
+  const existedUser = User.findOne({
+    $or: [{ email: email }],
+  });
+  if (existedUser) {
+    throw new apiErrors(409, "USer already exists");
+  }
+});
--- src/services/clodinary.js
+++ src/services/clodinary.js
@@ -1,8 +1,24 @@
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 
-   // Configuration
-   cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+const uploadCloudinary = async (file) => {
+  if (!file) {
+    return null;
+  }
+  try {
+    const result = await cloudinary.uploader.upload(file.path, {
+      resource_type: "auto",
+    });
+    return result;
+  } catch (error) {
+    fs.unlinkSync(file.path);
+    return null;
+  }
+};
+export default uploadCloudinary;
