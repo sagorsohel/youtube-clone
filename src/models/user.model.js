ca382af7b42141fb18e121e62fabac0506ce22b5
sagorsohel <sohel19sagor@gmail.com>
2025-03-08 22:08:25 +0600

    feat: implement password hashing and token generation in user model; remove password handling from video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -38,6 +40,44 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
+userSchema.methods.comparePassword = async function (candidatePassword, next) {
+  try {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch (error) {
+    return next(error);
+  }
+};
+userSchema.methods.generateAccessToken = function () {
+  const accessToken = jwt.sign(
+    { _id: this._id, email: this.email },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+  return accessToken;
+};
+userSchema.methods.generateRefreshToken = function () {
+  const refreshToken = jwt.sign(
+    { _id: this._id, email: this.email },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+  return refreshToken;
+};
 
 const User = mongoose.model("User", userSchema);
 export default User;
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -38,25 +38,9 @@ const videoSchema = new Schema(
   }
 );
 videoSchema.plugin(mongooseAggregatePaginate);
-videoSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  try {
-    const hashedPassword = await bcrypt.hash(this.password, 10);
-    this.password = hashedPassword;
-    return next();
-  } catch (error) {
-    return next(error);
-  }
-});
 
-videoSchema.methods.comparePassword = async function (candidatePassword, next) {
-  try {
-    const isMatch = await bcrypt.compare(candidatePassword, this.password);
-    return isMatch;
-  } catch (error) {
-    return next(error);
-  }
-};
+
+
 
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
