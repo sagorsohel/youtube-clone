52007b80d880c9e1185f1a496ec855daa1c10098
sagorsohel <sohel19sagor@gmail.com>
2025-03-09 07:25:06 +0600

    fix: improve JWT verification middleware with detailed error messages

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,22 +1,30 @@
 import User from "../models/user.model.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
+
 const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
       req.headers.authorization?.replace("Bearer ", "");
+
     if (!token) {
-      res.status(401).json({ message: "Unauthorized" });
+      return res.status(401).json({ message: "Unauthorized token not found" }); // Add return to stop execution
     }
-    // TODO: Implement JWT verification
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     req.user = await User.findById(decoded?._id).select(
       "-password -refreshToken"
     );
+
+    if (!req.user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
     next();
   } catch (error) {
-    res.status(401).json({ message: "Unauthorized" });
+    return res.status(401).json({ message: "Unauthorized" });
   }
 });
 
