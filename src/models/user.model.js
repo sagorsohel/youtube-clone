eca52961baa923266df5f1f21a2c54eaaa4d30ba
sagorsohel <sohel19sagor@gmail.com>
2025-03-10 03:44:49 +0600

    feat: add unique username field to user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -8,6 +8,14 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+      lowercase: true,
+      
+    },
     email: {
       type: String,
       required: true,
