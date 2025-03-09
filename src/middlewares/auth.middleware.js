2803480e5395274d1cf2dcbd6b2000007c058065
sagorsohel <sohel19sagor@gmail.com>
2025-03-10 02:23:56 +0600

    fix: add logging for JWT verification process and improve error handling

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,17 +7,20 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
     const token =
       req.cookies?.accessToken ||
       req.headers.authorization?.replace("Bearer ", "");
-
+    console.log("token", token);
     if (!token) {
       return res.status(401).json({ message: "Unauthorized token not found" }); // Add return to stop execution
     }
 
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log('decoded', decoded);
 
     req.user = await User.findById(decoded?._id).select(
       "-password -refreshToken"
     );
 
+    console.log(req.user);
     if (!req.user) {
       return res.status(401).json({ message: "User not found" });
     }
